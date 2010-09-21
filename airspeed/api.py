4046fb2c7cb5583f6528b6d43f1a2ad6db7647cc
Steve Purcell <steve@sanityinc.com>
2010-09-21 10:17:17 +0100

    Whitespace cleanup

--- airspeed/api.py
+++ airspeed/api.py
@@ -17,29 +17,29 @@ __all__ = ['Airspeed']
 
 class Airspeed(object):
     """The airspeed templating engine.
-    
+
     Sample usage:
-    
+
         >>> from cti.core import Engines
         >>> render = Engines()
         >>> render('airspeed:../tests/test.txt', dict())
         ('text/plain', 'Bingo!')
-    
+
     """
-    
+
     def __init__(self, cache=10, **kw):
         self.loaders = Cache(cache)
-    
+
     def __call__(self, data, template, mime_type="text/plain", **options):
         basepath = os.path.dirname(template)
-        
+
         if basepath not in self.loaders:
             loader = CachingFileLoader(basepath)
             self.loaders[basepath] = loader
-        
+
         else:
             loader = self.loaders[basepath]
-        
+
         template = self.loaders[basepath].load_template(os.path.basename(template))
-        
+
         return mime_type, template.merge(data, loader=loader)
