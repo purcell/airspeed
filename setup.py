56b1d80c4c7da3e54aefbba296a155612adbd8e1
Steve Purcell <steve@sanityinc.com>
2013-11-11 19:06:27 +0000

    Bump version, clean up whitespace

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ if sys.version_info <= (2, 1):
 
 setup(
         name = "airspeed",
-        version = "0.4.1",
-        
+        version = "0.4.2",
+
         description = "Airspeed is a powerful and easy-to-use templating engine for Python that aims for a high level of compatibility with the popular Velocity library for Java.",
         author = "Steve Purcell and Chris Tarttelin",
         author_email = "steve@pythonconsulting.com, chris@pythonconsulting.com",
@@ -22,10 +22,10 @@ setup(
         download_url = "http://pypi.python.org/pypi/airspeed/",
         license = "BSD",
         keywords = 'web.templating',
-        
+
         test_suite = 'nose.collector',
         tests_require = ['nose', 'coverage'],
-        
+
         classifiers = [
                 "Environment :: Web Environment",
                 "Intended Audience :: Developers",
@@ -35,11 +35,11 @@ setup(
                 "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
                 "Topic :: Software Development :: Libraries :: Python Modules"
             ],
-        
+
         packages = find_packages(exclude=['examples', 'tests', 'tests.*', 'docs']),
         include_package_data = False,
         zip_safe = True,
-        
+
         entry_points = {
                 'web.templating': [
                         'airspeed = airspeed.api:Airspeed',
