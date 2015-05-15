6d5bd99c89f1ff44b379fd4a4487c297e6e59527
self@dicos.ru <self@dicos.ru>
2015-05-15 12:04:43 +0600

    Use maxsize rather than max_size for cachetools
    fix bug: pp Airspeed()({'bars': bars}, 'template.html') *** TypeError: __init__() got an unexpected keyword argument 'max_size'

--- airspeed/api.py
+++ airspeed/api.py
@@ -25,7 +25,7 @@ class Airspeed(object):
     """
 
     def __init__(self, cache=10, **kw):
-        self.loaders = LRUCache(max_size=cache)
+        self.loaders = LRUCache(maxsize=cache)
 
     def __call__(self, data, template, mime_type="text/plain", **options):
         basepath = os.path.dirname(template)
